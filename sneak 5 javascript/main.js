85c0469520709c22fd0fdf967fcaafece00e3598
Antonello Viggiano <antonelloviggiano1990@hotmail.it>
2020-04-23 17:55:43 +0200

    soluzione alternativa con la funzione Math.pow(base, esponente);

--- sneak 5 javascript/main.js
+++ sneak 5 javascript/main.js
@@ -1,19 +1,25 @@
 // JSnack 5: Stampa il cubo dei primi N numeri, dove N è un numero indicato dall utente.
-// Chiedo all utente N
+// Chiedo all utente N ed esponente
 var n = prompt('Dammi il valore di N: ');
+var esponente = prompt('Dammi dell esponente cubo: ', 3);
 // mi servirà per stampare il mio output finale in una sola riga e non ripeterlo n volte
-var cuboinriga = [];
+// var cuboinriga = [];
+var outputinriga = [];
 // dato che devo ripetere una istruzione uso un ciclo
 for (var i = 1; i <= n; i++) {
     // stampa ogni singolo numero nel range del N ove il range parte da 1 a N
     console.log(i);
     // creo la variabile cubo
-    var cubo = i * i * i;
+    // var cubo = i * i * i;
+    var potenza = Math.pow(i, esponente);
     // stampo il cubo di ogni singolo numero nel range del N ove il range parte da 1 a N
-    console.log(cubo);
+    // console.log(cubo);
+    console.log(potenza);
     // se volessi stampare il risultato in una riga sola? uso l array
-    cuboinriga.push(cubo);
+    // cuboinriga.push(cubo);
+    outputinriga.push(potenza);
 }
 
 // stampa ogni cubo del singolo numero nel range del N su una sola riga
-console.log(cuboinriga);
+// console.log(cuboinriga)
+console.log(outputinriga);
--- sneak 6 javascript/main.js
+++ sneak 6 javascript/main.js
@@ -1,8 +1,8 @@
 // JSnack 4: Stampa le potenze di 2 fino a 1000.
 // dato che devo ripetere una istruzione dunque uso un ciclo
-// parto dalla base 2, controllo che sono dentro il range 1000 ,le potenze di due lo traduco in i = i  * 2
+// parto dalla base 1, controllo che sono dentro il range 1000 ,le potenze di due lo traduco in i = i  * 2
 var potenzedi2inriga = [];// mi servirà per stampare il mio output finale in una sola riga e non ripeterlo n volte
-for (var i = 2; i <= 1000; i = i * 2) {
+for (var i = 1; i <= 1000; i = i * 2) {
     // stampa ogni singolo numero scritto dall utente
     console.log(i);
     // se volessi stampare il risultato in una riga sola? uso l array
@@ -11,3 +11,8 @@ for (var i = 2; i <= 1000; i = i * 2) {
 
 // stampa ogni potenza del 2 ,fino a mille,su una sola riga
 console.log(potenzedi2inriga);
+// JSnack 4: Stampa le potenze di 2 fino a 1000.
+for (var i = 0; Math.pow(2, i) <= 1000 ; i++) {
+    var potenza = Math.pow(2, i);
+    console.log(potenza);
+}
