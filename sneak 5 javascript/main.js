6383928d02162de0b438a94a4579e63d3344ffe0
Antonello Viggiano <antonelloviggiano1990@hotmail.it>
2020-04-23 14:29:40 +0200

    stampa  i cubi dei primi  n numeri tutti su una linea

--- sneak 5 javascript/main.js
+++ sneak 5 javascript/main.js
@@ -1,6 +1,8 @@
 // JSnack 5: Stampa il cubo dei primi N numeri, dove N è un numero indicato dall utente.
 // Chiedo all utente N
 var n = prompt('Dammi il valore di N: ');
+// mi servirà per stampare il mio output finale in una sola riga e non ripeterlo n volte
+var cuboinriga = [];
 // dato che devo ripetere una istruzione uso un ciclo
 for (var i = 1; i <= n; i++) {
     // stampa ogni singolo numero nel range del N ove il range parte da 1 a N
@@ -9,9 +11,8 @@ for (var i = 1; i <= n; i++) {
     var cubo = i * i * i;
     // stampo il cubo di ogni singolo numero nel range del N ove il range parte da 1 a N
     console.log(cubo);
+    // se volessi stampare il risultato in una rica sola?
+    cuboinriga.push(cubo);
 }
 
-// sevolessi stampare il risultato in una rica sola?
-// var cuboinriga ='';
-// cuboinriga = cuboinriga + ' ' + cubo;
-// console.log(cuboinriga);
+console.log(cuboinriga);
